8bc5bd391ae32e9b3a796fa38c20b5a5fe83f023
Cristopher <alancristophercr@gmail.com>
2022-10-14 22:05:01 -0500

    pending to style component

---
+++ src/services/backend.js
@@ -0,0 +1,7 @@
+export const loginService = (email, password) => fetch(`${process.env.REACT_APP_BACKEND}/auth`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, password })
+});

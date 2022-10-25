ef99d1a0cacdc5229f2e57fe72253d72681156e9
rechy <rechy.mr@gmail.com>
2022-10-24 23:06:59 -0500

    addTemplate

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,12 +1,15 @@
 import { Container } from "@mui/material";
 import React from "react";
 import MyAccountContainer from "../components/MyAccount/MyAccountContainer";
+import Template from "../components/Template/Template";
 
 const MyAccount = () => {
   return (
-    <Container fixed>
-      <MyAccountContainer/>
-    </Container>
+    <Template>
+      <Container fixed>
+        <MyAccountContainer />
+      </Container>
+    </Template>
   );
 };
 

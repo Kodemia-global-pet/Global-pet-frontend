db615fee223fc091516beb33f37f9928c4366c61
Cristopher <alancristophercr@gmail.com>
2022-11-03 18:26:43 -0600

    deleted comments

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -2,21 +2,8 @@ import { Grid, Container } from "@mui/material";
 import React from "react";
 import Template from "../components/Template/Template";
 import UserFormRegister from "../components/UserFormRegister/UserFormRegister";
-// import { deleteTokenLocalStorage } from "../helpers/userHelper";
-// import { useLogedUser } from "../context/UserContext";
 
 const createAccount = () => {
-  // const { user, loading } = useLogedUser();
-  // const isAuth = user?.token;
-
-  // if (loading) {
-  //   return <h1>Loading..</h1>;
-  // } else {
-  //   if (isAuth) {
-  //     deleteTokenLocalStorage();
-  //     window.location.reload();
-  //   }
-  // }
   return (
     <Template>
       <Grid container>
